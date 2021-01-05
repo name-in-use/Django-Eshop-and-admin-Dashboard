bef732446bd7cc6f3d30fb6f725d8077b53b2c1f
Christos <xrhstoskr@outlook.com>
2021-01-05 05:49:01 -1100

    Admin panel: update order status manually

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_panel, name="admin_panel"),
+
+    path('update_order_status/', views.update_order_status, name="update_order"),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,35 +1,58 @@
 from django.shortcuts import render
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import json
 
 from store.models import *
 from users.models import *
 # Create your views here.
+
+
 def admin_panel(request):
-    
-    orders=OrderItem.objects.select_related('customer').all().order_by('customer')
-    
-    #total income
+
+    orders = OrderItem.objects.select_related(
+        'customer').all().order_by('customer')
+
+    # total income
     totalIncome = 0
     for product in OrderItem.objects.all():
-        productPrice=Product.objects.get(id=product.product_id).price
-        
+        productPrice = Product.objects.get(id=product.product_id).price
+
         Product_quantity = product.quantity
 
-        total=productPrice*Product_quantity
-        totalIncome+=total
-    print(totalIncome)
+        total = productPrice*Product_quantity
+        totalIncome += total
 
     totalProductsSelled = OrderItem.objects.values('order').count()
-    print('total products selled',totalProductsSelled)
-
     totalOrders = OrderItem.objects.values('order').distinct().count()
-    print('total orders',totalProductsSelled)
-    context={
-        'orders':orders,
-        'total_income':totalIncome,
-        'total_products_selled':totalProductsSelled,
-        'total_orders':totalOrders
-        
+
+    context = {
+        'orders': orders,
+        'total_income': totalIncome,
+        'total_products_selled': totalProductsSelled,
+        'total_orders': totalOrders
+
     }
 
     return render(request, 'index.html', context)
+
+
+def update_order_status(request):
+    data = json.loads(request.body.decode("utf-8"))
+
+    print(data['orderid'])
+    # orderID = OrderItem.objects.filter(order_id=data['orderid'])
+
+    # for order in orderID:
+    #    order = Order.objects.get(id=orderID)
+    #    order.complete = 1
+    #    order.save()
+
+    order_items = OrderItem.objects.filter(order_id=data['orderid'])
+
+
+    for order_item in order_items:
+       order = order_item.order
+       order.complete = True
+       order.save()
+    return JsonResponse(data)
